cfba0877174917ef9db4ff8e31ba1a07503a67e9
suwampy <tndhks123@nate.com>
2023-12-15 00:08:27 +0900

    초기세팅

---
+++ src/main/java/com/malog/MalogApplication.java
@@ -0,0 +1,13 @@
+package com.malog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalogApplication.class, args);
+    }
+
+}
