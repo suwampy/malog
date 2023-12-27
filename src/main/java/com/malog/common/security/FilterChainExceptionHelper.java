6c8dd689b1659c8419fa16eadd3d1c3f582600c9
suwampy <tndhks123@nate.com>
2023-12-28 01:30:44 +0900

    feat: 예외처리 통합 필터 구현

---
+++ src/main/java/com/malog/common/security/FilterChainExceptionHelper.java
@@ -0,0 +1,32 @@
+package com.malog.common.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+public final class FilterChainExceptionHelper extends OncePerRequestFilter {
+
+    private final HandlerExceptionResolver exceptionResolver;
+
+    public FilterChainExceptionHelper(@Qualifier("handlerExceptionResolver")
+    HandlerExceptionResolver exceptionResolver) {
+        this.exceptionResolver = exceptionResolver;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+        HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception exception) {
+            exceptionResolver.resolveException(request, response, null, exception);
+        }
+    }
+}
